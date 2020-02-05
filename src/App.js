6e7821afa6dec065a3c0061ea2d920eceb19b060
talmazov@gmail.com <hlorofil>
2020-02-05 16:22:39 +0200

    Start and Stop functions added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import TimerCounter from './components/timer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  reset(){
+    window.location.reload();
+  }
+  render() {
+
+    return (
+      <div>
+        <span>Sport:</span><TimerCounter />
+        <br/>
+        <span>Coding:</span><TimerCounter />
+        <br/>
+        <span>Work:</span><TimerCounter />
+        <br/>
+        <button onClick={this.reset}>Reset</button>
+      </div>
+    )
+  }
 }
-
 export default App;
---
+++ src/components/timer.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+
+class TimerCounter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hours: 0,
+            minutes: 0,
+            seconds: 0,
+
+        }
+        this.startCounting = this.startCounting.bind(this);
+        this.stopCounting = this.stopCounting.bind(this);
+    }
+
+
+    startCounting = (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+
+        this.refs.start.setAttribute("disabled", "disabled");
+        this.refs.pause.removeAttribute("disabled", "disabled");
+
+        this.interval = setInterval(() => {
+
+            if (this.state.seconds >= 0 && this.state.seconds < 59) {
+                this.setState({ seconds: this.state.seconds + 1 });
+            } else {
+
+                if (this.state.minutes >= 0 && this.state.minutes < 59) {
+
+                    this.setState({
+                        seconds: 0,
+                        minutes: this.state.minutes + 1
+                    })
+                } else {
+                    this.setState({
+                        minutes: 0,
+                        hours: this.state.hours + 1
+                    })
+                }
+            }
+
+        }, 1000);
+
+
+    }
+
+    stopCounting = (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        this.refs.start.removeAttribute("disabled", "disabled");
+        this.refs.pause.setAttribute("disabled", "disabled");
+        clearInterval(this.interval);
+
+    }
+    render() {
+
+        return (<div>
+            <div>
+                <button ref="start" onClick={this.startCounting}>
+                    Start
+            </button>
+                <button ref="pause" onClick={this.stopCounting}>Pause</button>
+            </div >
+            <div>
+                <span>{this.state.hours}</span>:
+                <span>{this.state.minutes}</span>:
+                <span>{this.state.seconds}</span>
+            </div>
+        </div>)
+    }
+
+}
+export default TimerCounter;
